8fc8479f9330211ae4d5502ce933373bd085d708
Mohamed Louli <14870254+mlouli@users.noreply.github.com>
2026-06-10 10:46:58 +0200

    Initial commit

---
+++ Spark/Spark/SparkApp.swift
@@ -0,0 +1,32 @@
+//
+//  SparkApp.swift
+//  Spark
+//
+//  Created by Mohamed Louli on 6/10/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct SparkApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ Spark/SparkTests/SparkTests.swift
@@ -0,0 +1,18 @@
+//
+//  SparkTests.swift
+//  SparkTests
+//
+//  Created by Mohamed Louli on 6/10/26.
+//
+
+import Testing
+
+struct SparkTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
